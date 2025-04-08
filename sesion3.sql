996368e94a7cf5965fae853ac624887cfba37eac
BENJAMIN SEBASTIAN LOPEZ HUIDOBRO <benjamin.lopezh@ucen.cl>
2025-04-08 18:22:55 -0400

    lab/se modifico el codigo de los bloques anonimos para que se calculen los gastos de cada cliente y se dividen en 3 categorias dependiendo del rango, tambien se documento el codigo.

--- sesion3.sql
+++ sesion3.sql
@@ -1,13 +1,20 @@
 --Escribe un bloque anónimo que calcule el total de alguna entidad y lo clasifique en 3 categorías: Alto, Medio o Bajo
 DECLARE
-v_total NUMBER := 600;
+	v_total_pedidos NUMBER;
+	v_cliente_id NUMBER := 1;
 BEGIN
-IF v_total > 1000 THEN
- DBMS_OUTPUT.PUT_LINE('Pedido grande: ' || v_total);
-ELSIF v_total > 500 THEN
- DBMS_OUTPUT.PUT_LINE('Pedido mediano: ' || v_total);
+	SELECT SUM(Total) INTO V_total_pedidos
+	FROM Pedidos
+	WHERE ClienteID = v_cliente_id;
+--El valor del pedido es mayor a 1000 es grande
+IF v_total_pedidos > 1000 THEN
+ DBMS_OUTPUT.PUT_LINE('Pedido grande: ' || v_total_pedidos);
+--El valor del pedido es mayor a 500 es mediano
+ELSIF v_total_pedidos > 500 THEN
+ DBMS_OUTPUT.PUT_LINE('Pedido mediano: ' || v_total_pedidos);
 ELSE
- DBMS_OUTPUT.PUT_LINE('Pedido pequeño: ' || v_total);
+--El valor del pedido es pequeño en cualquier otro caso
+ DBMS_OUTPUT.PUT_LINE('Pedido pequeño: ' || v_total_pedidos);
 END IF;
 END;
 /
